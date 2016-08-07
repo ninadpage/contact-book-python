272c12bb84d730cd51293b176c742be1ca01335b
ninadpage <ninad1902@gmail.com>
2016-08-08 00:23:29 +0200

    Removing githubpages extension from Sphinx conf

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -35,7 +35,6 @@ sys.path.insert(0, os.path.abspath('../contactbook'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
